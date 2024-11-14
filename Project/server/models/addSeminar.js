31c0473eb1c9671c00fe9c7a02049db4db343370
Sheraz <asherazali121823@gmail.com>
2024-11-14 16:37:13 +0500

    makee code according to test requirement

--- Project/server/models/addSeminar.js
+++ Project/server/models/addSeminar.js
@@ -2,7 +2,8 @@
 const mongoose = require("mongoose");
 const addSeminar = new mongoose.Schema(
   {
-    title: { type: String, required: true },
+    title: 
+    { type: String, required: true },
     description: { type: String, required: true},
     time: { type: String, required: true },
     paid: { type: String, required: true },
